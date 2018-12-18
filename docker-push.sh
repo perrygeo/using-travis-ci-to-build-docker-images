432bcdacf881d36de2b0b992883909b4b099bc31
Matthew Perry <perrygeo@gmail.com>
2018-12-18 09:52:42 -0700

    proper login

--- docker-push.sh
+++ docker-push.sh
@@ -2,7 +2,7 @@
 # Lots of assumptions about ENV VARS set in travis.yml
 set -e
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker tag $REPO:$COMMIT $REPO:latest
 docker tag $REPO:$COMMIT $REPO:$DATE-$COMMIT
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
