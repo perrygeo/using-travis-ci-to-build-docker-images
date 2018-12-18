8feccdde7db7fa0241328e214377719543d53e4c
Matthew Perry <perrygeo@gmail.com>
2018-12-18 10:09:32 -0700

    adjustments to tagging

--- docker-push.sh
+++ docker-push.sh
@@ -2,10 +2,7 @@
 # Lots of assumptions about ENV VARS set in travis.yml
 set -e
 
-DATE=`date +%Y%m%d`
-
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker tag $REPO:$COMMIT $REPO:latest
 docker tag $REPO:$COMMIT $REPO:$DATE
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
